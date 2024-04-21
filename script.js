a2a1ede9b75e9e0976599e8933ec5624b3cef5bc
Bogdan Petrut <bogdanpetrut.319@gmail.com>
2024-04-21 04:22:14 +0200

    Update game over screen in index.html and retry functionality in script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const game_over = document.querySelector('#game-over');
 const player = document.querySelector('#player');
+const obstacle = document.querySelector('.obstacle');
+
+let score = 0;
 
 function jump() {
     player.classList.add('hop');
@@ -7,9 +11,35 @@ function jump() {
     }, 700);
 }
 
+function retry() {
+    game_over.classList.add('hidden');
+    obstacle.style.animationPlayState = 'running';
+    player.style.animationPlayState = 'running';
+    score = 0;
+}
+
+function checkCollision() {
+    const playerBottom = parseInt(window.getComputedStyle(player).getPropertyValue('bottom'));
+    const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+    if (obstacleLeft < 150 && obstacleLeft > 40 && playerBottom <= 80) {
+        obstacle.style.animationPlayState = 'paused';
+        player.style.animationPlayState = 'paused';
+        game_over.classList.remove('hidden');
+    }
+}
+function checkScore() {
+    const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+    if (obstacleLeft < 40 && obstacleLeft > 39) {
+        score++;
+        document.querySelector('#score').textContent = score;
+    }
+}
+
 window.addEventListener('click', jump);
 window.addEventListener('keydown', function(e) {
     if (e.key === ' ' || e.key === 'ArrowUp') {
         jump();
     }
 });
+setInterval(checkCollision, 10);
+setInterval(checkScore, 20);
